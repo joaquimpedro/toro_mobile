d6f874af32180afe10df9ca239f95757c381634e
Joaquim Pedro <joaquim.pedro@hp.com>
2021-05-26 16:28:36 -0300

    criando repositório de stocks

---
+++ lib/repositories/stock_repository.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:toro_mobile/models/stock.dart';
+
+
+class StockRepository {
+
+  static const String BASE_URL = 'https://10.0.2.2:5001/api';
+  static const String TRENDS = '/trends';
+  static const String ORDER = '/order';
+
+  final dio = Dio();
+
+  Future<List<Stock>> fetchTrends() async {
+    var response = await dio.get(BASE_URL + TRENDS);
+    var results = response.data as List;
+
+    return results.map((json) => Stock.fromJson(json)).toList();
+  }
+
+  Future<Map<bool, String>> orderStock(Stock stock, int amount) async {
+    try {
+      var response = await dio.post(BASE_URL + ORDER,
+        data: {
+          'symbol': stock.symbol,
+          'amount': amount
+        }
+      );
+
+      return  {true: response.data};
+    } on DioError catch(e) {
+
+      if(e.response == null) {
+        return {false: 'Não foi possível acessar a Toro.'};
+      }
+
+      if(e.response!.statusCode == HttpStatus.badRequest) {
+        return  {false: e.response!.data};
+      }
+
+      return {false: e.message};
+    }
+  }
+
+ }
